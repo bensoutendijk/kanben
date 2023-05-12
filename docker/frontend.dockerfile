2b4f8f2fcc44f89af174b04bd8bd5da04d7e5329
Ben Soutendijk <bensoutendijk@gmail.com>
2023-05-12 10:33:59 -0500

    deyarn

--- docker/frontend.dockerfile
+++ docker/frontend.dockerfile
@@ -1,7 +1,3 @@
 FROM node:20-alpine AS development
 
 WORKDIR /app
-
-FROM development as build
-
-RUN yarn build
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { auth0 } from './auth0'
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { auth0 } from "./auth0";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
-app.use(auth0)
-app.mount('#app')
+app.use(router);
+
+app.mount("#app");
